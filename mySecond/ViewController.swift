9f8d56e0d06b30a1850de3c728f48668389ef2c2
Cousin <cousinlam@targa.com>
2015-12-29 14:55:21 +0800

    Initial Commit

---
+++ mySecond/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mySecond
+//
+//  Created by Cousin Lam on 29/12/15.
+//  Copyright (c) 2015 Targa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
